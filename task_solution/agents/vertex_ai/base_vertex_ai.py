f97052456e958a2005629a234e1ffde5b469fa68
fky1714 <417m520@gmail.com>
2025-06-17 20:33:41 +0900

    improve: LLMへの入出力ログの可視性を向上

--- task_solution/agents/vertex_ai/base_vertex_ai.py
+++ task_solution/agents/vertex_ai/base_vertex_ai.py
@@ -26,10 +26,11 @@ class BaseVertexAI:
         )
 
     def invoke(self, contents):
-        self.logger.info(f"contents > {contents[30:]}")
+        self.logger.info(f"contents > {contents[:50]}")
         response = self.model.generate_content(
             contents,
             generation_config=self.generation_config,
         )
-        self.logger.info(f"response > {response}")
+        output_res = response.text.replace("\n", " ")[:50]
+        self.logger.info(f"response > {output_res}")
         return response
